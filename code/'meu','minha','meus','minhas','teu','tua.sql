ed783789aef6ffa1997d7cf3187125ea2c0a9bb3
lorenypsum <loren.ypsum@gmail.com>
2021-12-20 21:59:02 -0300

    first commit

---
+++ code/'meu','minha','meus','minhas','teu','tua.sql
@@ -0,0 +1,8 @@
+'meu','minha','meus','minhas','teu','tua','teus','tuas','seu','sua','seus','suas','nosso','nossa','nossos','nossas','vosso','vossa',
+'vossos','vossas','seu','sua','seus','suas','o','a','os','as','mesmo','mesma','mesmos','mesmas','próprio','própria','próprios',
+'próprias','tal','tais','semelhante','semelhantes',
+'que','quem','qual','quais','quanto','quanta','quantos','quantas',
+'que','quem','onde','o qual','a qual','os quais','as quais','cujo','cuja','cujos','cujas','quanto','quanta','quantos','quantas',
+'alguém','ninguém','outrem','tudo','nada','cada','algo','algum','algumas','nenhuns','nenhuma','todo','todos','outra','outras',
+'muito','muita','pouco','poucos','certo','certa',
+'vários','várias','tanto','tantos','quanta','quantas','qualquer','quaisquer','bastante','bastantes','
